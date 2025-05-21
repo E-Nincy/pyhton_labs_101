2d59077923956362d46d4d323526dd25f4bc386b
E-Nincy <escotonincy10@outlook.es>
2025-05-21 16:59:39 +0800

    Took out text

--- python-101-main/05_machine-setup/05_02_yeehaw.py
+++ python-101-main/05_machine-setup/05_02_yeehaw.py
@@ -7,11 +7,6 @@ print("I´m a programmer now.")
 print("Yeehaw!")
 print("coding here i come")
 
-message ="""I am a programmer now!
-Yeehaw!
-Coding here I come!"""
-print(message)
-
 messages = [
     "I am a programmer now.",
     "Yeehaw!",

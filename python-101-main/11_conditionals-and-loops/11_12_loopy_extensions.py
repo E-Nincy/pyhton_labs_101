e3bd7a41355f77b1e1ebfc49e2f10f4cf12b61d1
E-Nincy <escotonincy10@outlook.es>
2025-05-23 12:48:25 +0800

    add text

--- python-101-main/11_conditionals-and-loops/11_12_loopy_extensions.py
+++ python-101-main/11_conditionals-and-loops/11_12_loopy_extensions.py
@@ -8,3 +8,31 @@
 # for example flags and conditional statements.
 
 filename = "operators.pdf"
+
+length = 0
+for _ in filename:
+    length += 1
+
+# I start with False because I haven’t found anything yet.
+dot_ok = False
+p_ok = False
+d_ok = False
+f_ok = False
+
+position = 0
+for char in filename:
+    if position == length - 4 and char == '.':
+        dot_ok = True
+    if position == length - 3 and char == 'p':
+        p_ok = True
+    if position == length - 2 and char == 'd':
+        d_ok = True
+    if position == length - 1 and char == 'f':
+        f_ok = True
+    position += 1
+
+# Result
+if dot_ok and p_ok and d_ok and f_ok:
+    print("This is a .pdf file.")
+else:
+    print("This is NOT a .pdf file.")

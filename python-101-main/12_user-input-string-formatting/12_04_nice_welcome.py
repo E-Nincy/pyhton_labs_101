eaf7e539880f0530d4372e655f8308946ea6ec49
E-Nincy <escotonincy10@outlook.es>
2025-05-26 17:23:56 +0800

    change text

--- python-101-main/12_user-input-string-formatting/12_04_nice_welcome.py
+++ python-101-main/12_user-input-string-formatting/12_04_nice_welcome.py
@@ -3,3 +3,13 @@
 # If a user enters more than one name, e.g. "firstname lastname",
 # then use only their first name to overstep some personal boundaries
 # in your welcome message.
+
+full_name = input("Please enter your name: ")
+first_name = full_name.split()[0]
+
+welcome = f"Hello!! {first_name}, is so nice you are here and \
+you can join me in this exersice, I thank you for beign this nice. \
+Have a nice day!!"
+
+print(welcome)
+

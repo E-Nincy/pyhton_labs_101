3a8ea6b783cb3b44a97164db119a8087393cd7d6
E-Nincy <escotonincy10@outlook.es>
2025-05-26 15:46:25 +0800

    add text

--- python-101-main/12_user-input-string-formatting/12_03_divisible.py
+++ python-101-main/12_user-input-string-formatting/12_03_divisible.py
@@ -1,3 +1,10 @@
 # Write a program that takes a number between 1 and 1,000,000,000
 # from the user and determines whether it is divisible by 3 using an `if` statement.
 # Print the result.
+
+number = int(input("Enter a number between 1 and 1,000,000,000: "))
+
+if number % 3 == 0:
+    print("Yay!! The number is divisible by 3.")
+else:
+    print("Oppss, The number is NOT divisible by 3.")

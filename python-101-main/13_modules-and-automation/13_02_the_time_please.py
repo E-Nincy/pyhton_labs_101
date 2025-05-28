c2387d1c050ecf9d74b2a0b94a7bfff0c85bb398
E-Nincy <escotonincy10@outlook.es>
2025-05-28 16:28:40 +0800

    add text

--- python-101-main/13_modules-and-automation/13_02_the_time_please.py
+++ python-101-main/13_modules-and-automation/13_02_the_time_please.py
@@ -1,2 +1,7 @@
 # Use a built-in Python module to tell you the current date and time.
 # Research online, so you can print it in a readable manner.
+
+from datetime import datetime
+
+now = datetime.now()
+print("Current date an time:", now)

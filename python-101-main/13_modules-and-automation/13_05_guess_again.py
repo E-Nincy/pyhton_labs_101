6c49b7b79905184c84e01fe572fd9d2c773ebd58
E-Nincy <escotonincy10@outlook.es>
2025-05-30 15:25:16 +0800

    inprove text

--- python-101-main/13_modules-and-automation/13_05_guess_again.py
+++ python-101-main/13_modules-and-automation/13_05_guess_again.py
@@ -1,3 +1,24 @@
 # Re-create the guess-my-number game from scratch. Don't peek!
 # This time, give your players only a certain amount of tries 
 # before they lose.
+
+import random
+
+num = random.randint(1, 10)
+guess = None
+attempts = 0
+max_attempts = 3
+
+while guess != num:
+    guess = int(input("Guess a number betwen 1 and 10: "))
+    
+    if guess == num:
+        print("Congratulations!! You won! .. nothing :P ")
+        break
+    else:
+        attempts += 1
+        print(f"Wrong guess. You have {max_attempts - attempts} tries left")
+    
+    if attempts == max_attempts:
+        print("Sorry you have already used all your tries :( ")
+        break

caac5f7db3563ac7a59d3a68c72baae3a43c40da
E-Nincy <escotonincy10@outlook.es>
2025-06-02 16:43:35 +0800

    add text

--- python-101-main/projects/mover.py
+++ python-101-main/projects/mover.py
@@ -12,3 +12,21 @@
 # Create a new path for each file
 
 # Move the screenshot there
+
+# Import pathlib
+import pathlib
+
+# Find the path to my Desktop
+home = pathlib.Path(r"C:\Users\escot\OneDrive\Escritorio")
+screenshots = home / "screenshots"
+
+# Create screenshots folder if it doesn't exist
+if not screenshots.exists():
+    screenshots.mkdir()
+
+# Move screenshots
+for filepath in home.iterdir():
+    if filepath.is_file() and filepath.suffix.lower() in [".jpg", ".png"]:
+        destination = screenshots / filepath.name
+        print("Moving:", filepath, "to", destination)
+        filepath.replace(destination)

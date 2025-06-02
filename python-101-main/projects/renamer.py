664518a78f0623c99b96d8e72e1719903fa6ea0a
E-Nincy <escotonincy10@outlook.es>
2025-06-02 17:37:00 +0800

    add text

--- python-101-main/projects/renamer.py
+++ python-101-main/projects/renamer.py
@@ -5,3 +5,22 @@
 # Create a new directory
 
 # Move and rename all screenshots
+
+
+import pathlib
+
+# Identify all screenshots on your Desktop
+screenshots = pathlib.Path(r"C:\Users\escot\OneDrive\Escritorio\screenshots")
+
+# Create a new directory
+destination = pathlib.Path(r"C:\Users\escot\OneDrive\Escritorio\renamed_screenshots")
+destination.mkdir(exist_ok=True)  # Create the folder if it doesn't exist
+
+# Loop through the files in the screenshots folder
+for index, filepath in enumerate(screenshots.iterdir()):
+    if filepath.suffix == ".jpg" or filepath.suffix == ".png":
+        # Create the new path in the destination folder
+        screenshot = destination.joinpath(str(index) + "_" + filepath.name)
+        print("Print result:", screenshot)
+        # Move and rename the file
+        filepath.replace(screenshot)
